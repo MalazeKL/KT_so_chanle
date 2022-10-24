c01247d96b4588599b1a08718cee5e3f3a5d7527
Your Name <Malaze0933@gmail.com>
2022-10-24 18:28:19 +0700

    kiem tra so chan le

---
+++ KT_so_chan_le.py
@@ -0,0 +1,8 @@
+num = float(input("nhập số cần KT: "))
+if num % 2 == 0:
+    print(num, " là số chẵn")
+elif num % 2 == 1:
+    print(num, " là số lẻ")
+else:
+    print(num, "không phải số tự nhiên")
+    
